eb5afd1a9509f66d2d72ce892c001ca0b9edf61d
tohruss <taraskind6@gmail.com>
2025-03-24 17:16:51 +0700

    create go and hello

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -28,4 +28,7 @@ function getConfigs(string $path = DIR_CONFIG): array
     return $settings;
 }
 
+require_once __DIR__ . '/../routes/web.php';
+
 return new Src\Application(new Src\Settings(getConfigs()));
+
